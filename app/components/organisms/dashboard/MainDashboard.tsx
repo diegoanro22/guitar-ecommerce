dac6d8f651d11a35e3e73fcae4c6673e221d5de2
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-22 20:23:55 -0600

    feat: :sparkles: create GuitarCard component for the dashboard page

---
+++ app/components/organisms/dashboard/MainDashboard.tsx
@@ -0,0 +1,9 @@
+import GuitarGrid from '@molecules/dashboard/GuitarCard';
+
+export default function MainDashboard() {
+  return (
+    <main className="mt-21 sm:mt-26 md:mx-20 xl:mx-35">
+      <GuitarGrid />
+    </main>
+  );
+}
--- app/components/templates/Dashboard.tsx
+++ app/components/templates/Dashboard.tsx
@@ -1,10 +1,12 @@
 import Header from '@organisms/shared/Header';
 import Footer from '@organisms/shared/Footer';
+import MainDashboard from '@organisms/dashboard/MainDashboard';
 
 export default function DashboardPage() {
   return (
     <div className="bg-background min-h-screen">
       <Header />
+      <MainDashboard />
       <Footer />
     </div>
   );
